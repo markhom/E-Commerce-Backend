66b4cf4a7158d2aadf1f0c18ed1cbd36406676aa
Mark Homchik <markhomchik4@gmail.com>
2024-05-30 23:25:20 -0500

    category.js

--- db/models/Category.js
+++ db/models/Category.js
@@ -5,8 +5,18 @@ const sequelize = require('../config/connection.js');
 class Category extends Model {}
 
 Category.init(
+  //Create Columns
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name:  {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
